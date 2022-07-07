628db6c91ae8b7eb93c6c3b00be35bed91ace721
dgane02 <gane.damien971@gmail.com>
2022-07-07 15:13:25 -0400

    V6

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,12 +2,7 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Formulaires;
-use App\Entity\Questions;
 use App\Entity\User;
-use App\Repository\FormulairesRepository;
-use App\Repository\QuestionsRepository;
-use App\Repository\UserRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -18,38 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
-    /**
-     * @var UserRepository
-     */
-    protected UserRepository $userRepository;
-
-    /**
-     * @var FormulairesRepository
-     */
-    protected FormulairesRepository $formulairesRepository;
-
-    /**
-     * @var QuestionsRepository
-     */
-    protected QuestionsRepository $questionsRepository;
-
-    public function __construct(
-        UserRepository $userRepository,
-        FormulairesRepository $formulairesRepository,
-        QuestionsRepository $questionsRepository
-    )
-    {
-        $this->userRepository = $userRepository;
-        $this->formulairesRepository = $formulairesRepository;
-        $this->questionsRepository = $questionsRepository;
-    }
-
-    public function createEntity(string $entityFqcn)
-    {
-        $formulaire = new Formulaires;
-        $formulaire-> setTitle('Formulaire');
-    }
-
+    private $userRepository;
 
     /**
      * @Route("/admin", name="admin")
@@ -58,8 +22,7 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
         return $this -> render('bundles/EasyAdminBundle/welcome.html.twig', [
-            'countAllUser' => $this->userRepository->countAllUser(),
-            'countAllFormulaires' => $this->formulairesRepository->countAllFormulaires()
+            'countAllUser' => [],
 
         ]);
     }
@@ -80,41 +43,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
-        yield MenuItem::linkToCrud('Formulaires', 'fas fa-list', Formulaires::class);
         yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-list', User::class);
-        yield MenuItem::linkToCrud('Questions', 'fas fa-list', Questions::class);
 
-    }
-
-    /**
-     * @return UserRepository
-     */
-    public function getUserRepository(): UserRepository
-    {
-        return $this->userRepository;
-    }
-
-    /**
-     * @param UserRepository $userRepository
-     */
-    public function setUserRepository(UserRepository $userRepository): void
-    {
-        $this->userRepository = $userRepository;
-    }
-
-    /**
-     * @return FormulairesRepository
-     */
-    public function getFormulairesRepository(): FormulairesRepository
-    {
-        return $this->formulairesRepository;
-    }
-
-    /**
-     * @param FormulairesRepository $formulairesRepository
-     */
-    public function setFormulairesRepository(FormulairesRepository $formulairesRepository): void
-    {
-        $this->formulairesRepository = $formulairesRepository;
     }
 }
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,7 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use phpDocumentor\Reflection\Types\Boolean;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +19,35 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+    // configureActions to show properties in the list view of a user
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions -> add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
+
+    /**
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        // if admin, show only nom, prenom, email
+        if ($this -> isGranted('ROLE_ADMIN')) {
+            return [
+                TextField::new('nom', 'Nom'),
+                TextField::new('prenom', 'Prenom'),
+                TextField::new('email', 'Email'),
+            ];
+        } else {
+            return [
+                TextField::new('nom'),
+                TextField::new('prenom'),
+                TextField::new('email'),
+                TextField::new('telephone'),
+                TextField::new('adresseDesParents'),
+                // if the user is admin, no need to show the field
+                BooleanField::new('candidaterAutresFormations', 'Candidater à d\'autres formations ?')->renderAsSwitch(false)
+
+            ];
+        }
     }
-    */
+     */
 }
+
